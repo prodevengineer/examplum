262b3defdf76d30adb9f662d34cee35f6bdd82b3
prodevengineer <64164769+prodevengineer@users.noreply.github.com>
2020-09-09 11:49:56 -0700

    iframe reconfiguration

---
+++ m3c/data.js
@@ -0,0 +1,42 @@
+module.exports=[
+  {
+    id:"GykTLqODQuU",
+    title: "Formulario Animado com JS puro e CSS | Mayk Brito",
+    duration: "57 min",
+    price: "Free",
+    featured: true
+  },
+  {
+    id:"HN1UjzRSdBk",
+    title: "Desvendando o CSS Grid na pratica| Mayk Brito",
+    duration: "36 min",
+    price: "Free"
+  },
+  {
+    id:"rAzHvYnQ8DY",
+    title: "Array: High Order Functions | Mayk Brito",
+    duration: "54 min",
+    price: "Free"
+  },
+  {
+    id:"ghTrp1x_1As",
+    title: "O que eh API? REST e RESTFUL? | Mayk Brito",
+    duration: "33 min",
+    price: "Free",
+    featured:true
+  },
+  {
+    id:"GSqR2i-Pq6o",
+    title: "Desvendando a variavel THIS | Mayk Brito",
+    duration: "48 min",
+    price: "Free"
+  },
+  {
+    id:"2alg7MQ6_sI",
+    title: "Como usar Git e GitHub na pratica: Guia para iniciantes | Mayk Brito",
+    duration: "33 min",
+    price: "Free",
+    featured:true
+  }
+
+]
